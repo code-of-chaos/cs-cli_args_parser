e2e298e19d41e15350dcb95f85c149f75040ebbb
Anna Sas <anna.sas.dev@gmail.com>
2024-12-12 22:51:46 +0100

    Feat: Import of Library commands

---
+++ src/CodeOfChaos.CliArgsParser.Library/CommandAtlases/VersionBumpParameters.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CliArgsParser;
+
+namespace CodeOfChaos.CliArgsParser.Library.CommandAtlases;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class VersionBumpParameters : ICommandParameters {
+    [ArgValue("root")] [Description("The root directory of the project to update")]
+    public string Root { get; set; } = "../../../../../";
+    
+    [ArgValue("section")] [Description("The section of the version to bump. One of: Major, Minor, Patch")]
+    public string? SectionStringValue { get; set; }
+    
+    [ArgValue("projects")] [Description("The projects to update")]
+    public string? ProjectsStringValue { get; set; }
+    
+    [ArgValue("project-split")] [Description("The split character to use when splitting the projects string")]
+    public string ProjectsSplit { get; set; } = ";";
+    
+    [ArgValue("source-folder")] [Description("The folder where the projects are located")]
+    public string SourceFolder { get; set; } = "src";
+    
+    private string[]? _projects;
+    public string[] Projects => _projects ??= ProjectsStringValue?
+            .Split(ProjectsSplit)
+            .Where(entry => !string.IsNullOrWhiteSpace(entry))
+            .ToArray()
+        ?? [];
+
+
+    public VersionSection Section => Enum.Parse<VersionSection>(SectionStringValue ?? "None", ignoreCase:true);
+}
+
+public enum VersionSection {
+    Major,
+    Minor,
+    Patch,
+    None
+}
---
+++ src/Tools.CodeOfChaos.CliArgsParser/Program.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CliArgsParser;
+using CodeOfChaos.CliArgsParser.Library;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tools.CodeOfChaos.CliArgsParser;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class Program {
+    public async static Task Main(string[] args) {
+        IServiceCollection serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddCliArgsParser(configuration =>
+            configuration
+                .SetConfig(new CliArgsParserConfig {
+                    Overridable = true,
+                    GenerateShortNames = true
+                })
+                .AddFromAssembly(typeof(IAssemblyEntry).Assembly)
+        );
+
+        ServiceProvider provider = serviceCollection.BuildServiceProvider();
+
+        var argsParser = provider.GetRequiredService<IArgsParser>();
+        await argsParser.ParseAsyncLinear(args);
+    }
+}
