be8cc839eba643d94207616678eb5e08058a1388
Anna Sas <anna.sas.dev@gmail.com>
2024-12-12 23:26:48 +0100

    Update VersionBumpParameters.cs

--- src/CodeOfChaos.CliArgsParser.Library/CommandAtlases/VersionBumpParameters.cs
+++ src/CodeOfChaos.CliArgsParser.Library/CommandAtlases/VersionBumpParameters.cs
@@ -9,19 +9,19 @@ namespace CodeOfChaos.CliArgsParser.Library.CommandAtlases;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public class VersionBumpParameters : ICommandParameters {
-    [ArgValue("root")] [Description("The root directory of the project to update")]
+    [ArgValue("root", "r")] [Description("The root directory of the project to update")]
     public string Root { get; set; } = "../../../../../";
     
-    [ArgValue("section")] [Description("The section of the version to bump. One of: Major, Minor, Patch")]
+    [ArgValue("section", "s")] [Description("The section of the version to bump. One of: Major, Minor, Patch")]
     public string? SectionStringValue { get; set; }
     
-    [ArgValue("projects")] [Description("The projects to update")]
+    [ArgValue("projects", "p")] [Description("The projects to update")]
     public string? ProjectsStringValue { get; set; }
     
-    [ArgValue("project-split")] [Description("The split character to use when splitting the projects string")]
+    [ArgValue("project-split", "ps")] [Description("The split character to use when splitting the projects string")]
     public string ProjectsSplit { get; set; } = ";";
     
-    [ArgValue("source-folder")] [Description("The folder where the projects are located")]
+    [ArgValue("source-folder", "sf")] [Description("The folder where the projects are located")]
     public string SourceFolder { get; set; } = "src";
     
     private string[]? _projects;
